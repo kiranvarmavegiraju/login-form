7f32402c79de5dc8f77206f1caf19d1530155ecb
kiranvarmavegiraju <59106768+kiranvarmavegiraju@users.noreply.github.com>
2020-07-03 13:18:18 +0530

    Initial commit

---
+++ login.js
@@ -0,0 +1,14 @@
+
+
+function fun2()
+{
+    document.getElementById('login').style.left="450px";
+    document.getElementById('register').style.left="50px";
+    document.getElementById('btn').style.left="110px";
+}
+function fun1()
+{
+    document.getElementById('login').style.left="50px";
+    document.getElementById('register').style.left="450px";
+    document.getElementById('btn').style.left="0px";
+}
